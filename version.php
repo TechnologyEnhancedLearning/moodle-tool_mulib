58f0ef09723f21f9abaa07ad309c1fe0132a7cbb
Petr Skoda <petr@mutms.org>
2025-04-14 11:18:23 +0200

    Release mu-4.5.4-01

--- version.php
+++ version.php
@@ -28,9 +28,9 @@ defined('MOODLE_INTERNAL') || die();
 
 /** @var stdClass $plugin */
 $plugin->component = 'tool_mulib';
-$plugin->version   = 2025040900;
+$plugin->version   = 2025041400;
 $plugin->requires  = 2024100700;
 $plugin->maturity  = MATURITY_ALPHA;
 $plugin->supported = [405, 405];
-$plugin->release   = 'mu-4.5.3-04';
+$plugin->release   = 'mu-4.5.4-01';
 
